e3b21d57c5c3a83bec2bf487ea49eb352c0f1774
Samuel Oaks <samuelclayoaks@yahoo.com>
2023-07-12 17:44:39 -0400

    added grafbase functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,24 @@
-import Footer from '@/components/Footer'
-import './globals.css'
-import Navbar from '@/components/Navbar'
+import Footer from "@/components/Footer";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: 'Flexible',
-  description: 'Showcase and discover remarkable developer projects',
-}
+  title: "Flexible",
+  description: "Showcase and discover remarkable developer projects",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main>
-        {children}
-        </main>
+        <main>{children}</main>
         <Footer />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
