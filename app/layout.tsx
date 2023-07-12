500de42288b22e4492efe6c0e652ffa7edba2816
Samuel Oaks <samuelclayoaks@yahoo.com>
2023-07-12 17:27:13 -0400

    created github repo

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import Footer from '@/components/Footer'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+
+export const metadata = {
+  title: 'Flexible',
+  description: 'Showcase and discover remarkable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+        {children}
+        </main>
+        <Footer />
+        </body>
+    </html>
+  )
+}
